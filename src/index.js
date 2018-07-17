284a1a144f69168dc50439d77fded187e25bd34f
Nathaniel Diamond <nathanieldiamond@Nathaniels-MacBook-Pro.local>
2018-07-16 23:09:20 -0700

    Turns done my way working ahead with if/else

--- src/index.js
+++ src/index.js
@@ -2,25 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = { value: null};
-  }
-
-  render() {
+function Square(props) {
     return (
       <button className="square"
-       onClick= {() => this.setState({value: 'X'})}>
-        {this.state.value}
+       onClick= {() => props.onClick()}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(this.state.turn == 'X') {
+      squares[i] = 'X';
+      this.setState({squares: squares, turn: 'O'});
+    } else {
+      squares[i] = 'O';
+      this.setState({squares: squares, turn: 'X'});
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {squares: Array(9).fill(null),
+                  turn: 'X'};
+  }
+
   renderSquare(i) {
-    return <Square value = {i} />;
+    return <Square value = {this.state.squares[i]}
+            onClick={()=>this.handleClick(i)}/>;
   }
 
   render() {
